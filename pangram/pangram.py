2e7c5fd59def65c489d0ca8950a29a7b12e4dd03
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-09-30 12:37:50 +0200

    pangram done

---
+++ pangram/pangram.py
@@ -0,0 +1,52 @@
+def is_pangram(string):
+  pangram = True
+
+  alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ]
+
+  letters = []
+
+  for letter in alphabet:
+    if letter not in string.lower():
+      pangram = False
+  return pangram
+
+
+
+
+
+
+
+
+
+
+
+
+
+
