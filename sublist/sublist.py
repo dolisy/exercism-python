d46a8aa70954493648a30c24832fab1f00c0eec2
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-10-16 13:35:47 +0200

    sublist done

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,2 +1,34 @@
+SUBLIST = 1
+SUPERLIST = 2
+EQUAL = 3
+UNEQUAL = 4
+
+
 def check_lists(first_list, second_list):
-    pass
+  if first_list == second_list:
+    return EQUAL
+
+  if len(first_list) <= len(second_list):
+    lists = {
+      'A': first_list,
+      'B': second_list,
+      True: SUBLIST
+    }
+  else:
+    lists = {
+      'B': first_list,
+      'A': second_list,
+      True: SUPERLIST
+    }
+
+  count = 0
+  for i in range(0, len(lists['B']) - len(lists['A']) + 1):
+    count = 0
+    for j in range(0, len(lists['A'])):
+      if lists['B'][i + j] == lists['A'][j]:
+        count += 1
+    if count == len(lists['A']):
+      return lists[True]
+
+  return UNEQUAL
+
