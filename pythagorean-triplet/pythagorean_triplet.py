224bd8ee239ca4714f9da83ccbdd5af62a45cc47
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-10-17 17:07:53 +0200

    pythagorean triplet done

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,10 +1,15 @@
 def primitive_triplets(number_in_triplet):
-    pass
-
+  if number_in_triplet % 4 == 0:
+    return set([(sorted([m**2 - n**2, 2*m*n, m**2 + n**2])[0], sorted([m**2 - n**2, 2*m*n, m**2 + n**2])[1], sorted([m**2 - n**2, 2*m*n, m**2 + n**2])[2]) for m in range(0, number_in_triplet) for n in range(0, number_in_triplet) if 2*m*n == number_in_triplet and m - n > 0 and (m - n) % 2 == 1 and (m % n != 0 or n == 1) and (n % m != 0 or m == 1)])
+  else:
+    raise ValueError
 
 def triplets_in_range(range_start, range_end):
-    pass
-
+  return set([(sorted([a,b,c])[0], sorted([a,b,c])[1], sorted([a,b,c])[2]) for a in range(range_start, range_end + 1) for b in range(range_start, range_end + 1) for c in range(range_start, range_end + 1) if a**2 + b**2 == c**2])
 
 def is_triplet(triplet):
-    pass
+  if len([x for x in triplet if x % 4 == 0]) == 1:
+    return sorted(list(triplet)) in [sorted(list(x)) for x in primitive_triplets([x for x in triplet if x % 4 == 0][0])]
+  else:
+    return False
+
