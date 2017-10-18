fa9bf73f7a0925df9355fb327622e2a88e432a17
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-10-18 11:29:14 +0200

    pig latin

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -6,13 +6,34 @@ def rule_1(text):
 def rule_2(text, consonant):
   return re.sub(consonant, '', text, count=1) + consonant + 'ay'
 
-def translate(text):
+def translate_word(text):
+
+  text = text.lower()
   vowels = ['a', 'e', 'i', 'o', 'u']
+  exceptions = ['y', 'x', 'squ', 'qu', 'ch', 'thr', 'th', 'ch', 'sch'] # because every rule have their exceptions
 
-  for i in range(0, len(text)):
-    if
+  for exception in exceptions:
+    if text.startswith(exception):
+      if text.replace(exception, '')[0] in vowels:
+        return rule_2(text, exception)
+      else:
+        return rule_1(text)
 
+  for vowel in vowels:
+    if text.startswith(vowel):
+      return rule_1(text)
 
-print translate("xray") #, "xrayay"
+  consonant = ''
+  for letter in text:
+    if letter not in vowels:
+      consonant += letter
+    else:
+      break
+  return rule_2(text, consonant)
 
+def translate(sentence):
+  text = []
+  for word in sentence.lower().split(' '):
+    text.append(translate_word(word))
 
+  return ' '.join(text)
